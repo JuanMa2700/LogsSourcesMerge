1a470ae72d9e87941cc1db534c2cbf8ffc07b780
Juan Martin Zuluaga Acevedo <juamazuace@gmail.com>
2024-09-10 13:38:19 -0500

    feat: improve performance fetching all logs with promise all at the beginning

--- src/solution/async-sorted-merge.ts
+++ src/solution/async-sorted-merge.ts
@@ -2,20 +2,20 @@ import Heap from "heap";
 import { LogEntryType, LogSourceType } from "../lib/log-source";
 import { PrinterType } from "../lib/printer";
 
-/* When dealing with a small number of log sources (e.g., two or three), 
-a straightforward approach is to compare the current log entries from 
-each source and print the smallest one. This approach involves a linear 
-comparison of the logs from all sources, which can be manageable with 
+/* When dealing with a small number of log sources (e.g., two or three),
+a straightforward approach is to compare the current log entries from
+each source and print the smallest one. This approach involves a linear
+comparison of the logs from all sources, which can be manageable with
 a small number of sources.
 However, as the number of log sources increases, this comparison method
-becomes less efficient. The time complexity of comparing each log from 
-every source becomes impractical, as it scales poorly with the number 
+becomes less efficient. The time complexity of comparing each log from
+every source becomes impractical, as it scales poorly with the number
 of sources and log entries.
-For a large number of log sources, a more efficient solution is to use 
-a min-heap. A min-heap is a priority queue that always provides access 
-to the smallest element efficiently. 
-In summary, for a small number of sources, direct comparison can be 
-sufficient, but for a large number of sources, a min-heap offers a 
+For a large number of log sources, a more efficient solution is to use
+a min-heap. A min-heap is a priority queue that always provides access
+to the smallest element efficiently.
+In summary, for a small number of sources, direct comparison can be
+sufficient, but for a large number of sources, a min-heap offers a
 more scalable and efficient solution by optimizing the organization and
 retrieval of log entries.*/
 
@@ -40,21 +40,39 @@ const asyncMergeLogSources = (
         }
       });
 
+      const buffer: Record<string, LogEntryType[]> = {};
+      logSources.forEach((_, i) => {
+        buffer[i] = [];
+      });
+
+      let existsLogs = true;
+
+      while (existsLogs) {
+        const newLogs = await Promise.all(
+          logSources.map((source) => source.popAsync())
+        );
+        newLogs.forEach((l, index) => {
+          if (l)
+            buffer[index].push({ ...(l as LogEntryType), sourceIndex: index });
+        });
+        if (newLogs.every((l) => !l)) existsLogs = false;
+      }
+
       while (minHeap.size() > 0) {
         const smallest = minHeap.pop();
         if (smallest) {
           printer.print(smallest);
 
-          const smallestLogSource = logSources[smallest.sourceIndex!];
-          const nextLog = await smallestLogSource.popAsync();
+          const smallestLogSource = buffer[smallest.sourceIndex!];
+          const nextLog = smallestLogSource.shift();
           if (nextLog) {
             minHeap.push({
               ...(nextLog as LogEntryType),
-              sourceIndex: smallest.sourceIndex,
             });
           }
         }
       }
+
       resolve(printer.done());
     } catch (e) {
       console.log(`////// ERROR: ${e} //////`);
